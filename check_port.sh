77277bae05f206022b52befd5e336b202e17d1f9
AliReza <hamzehzadehalireza@gmail.com>
2023-03-11 18:54:39 +0400

    second commit

--- check_port.sh
+++ check_port.sh
@@ -1,7 +1,12 @@
 !#/bin/bash
-server_ip=154.91.170.54
+
+server_ip=localhost
 server_port=443
-if (echo > /dev/tcp/$server_ip/$server_port) &>/dev/null
+
+echo $server_ip
+echo $server_port
+
+if (echo >/dev/tcp/$server_ip/$server_port) &>/dev/null
 then
   echo "port is open ..."
 else
